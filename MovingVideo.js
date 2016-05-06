8a8faed10df2604ccbda58cdb58ab5930fd91bc8
anthonycaulkins <acaulkin@uci.edu>
2016-05-06 00:25:23 -0700

    Update MovingVideo.js
    This code does not get errors when running, but the jit.qt.movie takes up the entire jit.pwindow and I'm not sure why.

--- MovingVideo.js
+++ MovingVideo.js
@@ -5,10 +5,10 @@ outlets = 1;
 var fullMatrix = new JitterMatrix(4,"char", 900, 900);
 var riverMovie = new JitterObject("jit.qt.movie",240,240);
 
-var source_start = [0, 0]; //There is an error saying "Matrix Array Argument Not Valid Matrix or Matrixname", but I am not sure why.
-var source_end = [480, 480];
-var dim_start = [0, 240];
-var dim_end = [240, 480];
+var source_start = [0,0];
+var source_end = [480,480];
+var dim_start = [0,240];
+var dim_end = [240,480];
 
 //movie attribue values 
 riverMovie.vol = 0;
@@ -31,18 +31,17 @@ function read(filename)
 
 function bang()
 {
-    
-    riverMovie.matrixcalc(riverMovie,fullMatrix); //I suspect that this is not doing what I am imagining it might. 
+    fullMatrix.clear();
 
     fullMatrix.usesrcdim = 1;
     fullMatrix.srcdimstart = source_start;
     fullMatrix.srcdimend = source_end;
 
-    fullMatrix.clear(); //I'm not sure if this is the way to have it clear the matrix with each incoming bang.
-
     fullMatrix.usedstdim = 1;
     fullMatrix.dstdimstart = dim_start;
     fullMatrix.dstdimend = dim_end;
+  
+    riverMovie.matrixcalc(fullMatrix,fullMatrix); //Changing this worked to get rid of the error messages. 
 
 	
     outlet(0, "jit_matrix", fullMatrix.name);
