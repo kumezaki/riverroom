7f07bd072b897fcc4ef17e64d6f7fb60ffea31ce
anthonycaulkins <acaulkin@uci.edu>
2016-06-03 19:26:35 -0700

    -Multiple videos at different Y heights, working. -Still needing to get videos to move across screen.

--- MovingGL.js
+++ MovingGL.js
@@ -1,46 +1,39 @@
 autowatch = 1;
 inlets = 1;
 
-var movingvideos = new JitterObject("jit.window","movingvideos");
-movingvideos.depthbuffer = 0;
-movingvideos.idelmouse = 1;
+var context_name = "MovingVideos";
 
-var myrender = new JitterObject("jit.gl.render","movingvideos");
+var mywindow = new JitterObject("jit.window",context_name);
+mywindow.depthbuffer = 0;
+mywindow.idelmouse = 1;
+
+var myrender = new JitterObject("jit.gl.render",context_name);
 myrender.ortho = 2;
 myrender.erase_color = [0,0,0,1];
 
-var videoH = 240;
-var videoW = 240;
-
-var fullMatrix = new JitterMatrix(4,"char", 900, 900);
-var smallMatrix = new JitterMatrix(4,"char",videoH, videoW);
-
+var mymoviemat = new JitterMatrix(4,"char",480,480);
  
 var numMovies = 3; // AC: Set to the total number of videos
 var txtFile = "MovieFileNames.txt";
 var file = new File(txtFile); 
 
 var riverMovie = new Array;
+var movieplane = new Array;
 
 var initY = new Array;
 var changeX = new Array;
 var videoX = new Array;
 
-//var mylistener = new JitterListener(movingvideos.getregisteredname()); //don't fully understand this
-
 function init_float_array_random()
 {
 	for (i = 0; i < numMovies; i++)
 	{
-	   initY[i] = Math.random()*(fullMatrix.dim[0]-240); 
-	
-	   changeX[i] = (Math.random()*5)+1; 
-	
-	   videoX[i] = 0; 
+	   initY[i] = (Math.random()*(-1))+Math.random();
+       post("Y pos" + "" + initY[i] + "\n");
+	   changeX[i] = Math.random()*0.25; 
+	   videoX[i] = -1.5; 
 	}
 }
-
-var movieplane = new Array;
 
 function loadbang()
 {
@@ -57,10 +50,9 @@ function loadbang()
 		riverMovie[i].read(moviename[i]);
 		riverMovie[i].vol = 0.;
 		riverMovie[i].engine = "avf";
-		movieplane[i] = new JitterObject("jit.gl.videoplane","movingvideos");
-		movieplane[i].drawto = "movingvideos";
+		movieplane[i] = new JitterObject("jit.gl.videoplane",context_name);
 		movieplane[i].scale = [0.25,0.25,1.];
-        movieplane[i].position = [0.,initY[i],0.]; //I know this isn't quite right
+        movieplane[i].position = [videoX[i],initY[i],0.];
   	}
 	
 	file.close();
@@ -70,22 +62,17 @@ function bang()
 {
   for (i = 0; i < numMovies; i++)
   {
-    riverMovie[i].matrixcalc(smallMatrix,smallMatrix);
-    movieplane[i].matrixcalc(smallMatrix,smallMatrix);
-  
-    fullMatrix.frommatrix(smallMatrix);
+    riverMovie[i].matrixcalc(mymoviemat,mymoviemat);
+    movieplane[i].jit_matrix(mymoviemat.name);
 	}
+	
+//	movieplane[i].position = [changeX[i]
+    myrender.erase(); // erase the drawing context
+	myrender.drawclients(); // draw the client objects
+	myrender.swap(); // swap in the new drawing
 }
 
 function fullscreen(v)
 {
-	movingvideos.fullscreen = v;
-}
-
-function thecallback()
-{
-}
-
-function thecallback()
-{
+	mywindow.fullscreen = v;
 }
